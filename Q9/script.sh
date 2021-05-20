e670ef97fadba477ed8c01aa84001961bfc8f608
Faraz <farazattari786@gmail.com>
2021-05-20 15:23:00 +0530

    first commit
    create user and change UID using shell script

---
+++ Q9/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+choice="no"
+while [ "$choice" != "yes" ]; do
+    echo -n -e "\nEnter a new user name: "
+    read username
+    echo -n "Enter a number between 667 to 687 as UID: "
+    read uid
+    echo -e "\nIs the following information correct?"
+    echo -e "\tUser Name: $username"
+    echo -e "\tThe entered UID is: $uid"
+    echo -n -e "Enter \"yes\" if the information is correct: "
+    read choice
+done
+useradd $username
+usermod -u $uid $username
+userdetails=$(getent passwd "$username")
+echo -e "\n"
+echo "$userdetails"
