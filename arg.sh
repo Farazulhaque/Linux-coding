c53696b67cee0598d6b15ecb436b3084769666e7
Faraz <farazattari786@gmail.com>
2021-06-23 23:09:05 +0530

    modified args.sh

--- arg.sh
+++ arg.sh
@@ -4,3 +4,11 @@ echo "The command line arguments are: "
 for i; do
     echo $i
 done
+
+#!/bin/bash
+
+echo "Command line arguments are: "
+for i; do
+	echo $i
+done
+echo 'Total number of arguments entered: ' $#
