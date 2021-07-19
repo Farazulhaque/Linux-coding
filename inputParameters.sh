9364db6dd18a720808a4e00ebf2e39b823d9cff4
Faraz <farazattari786@gmail.com>
2021-07-19 07:05:11 +0530

    inputparameters and process id

---
+++ inputParameters.sh
@@ -0,0 +1,13 @@
+if [ $# -eq 4 ];
+then
+	echo 'Command Executed: ' $0
+	echo 'First input: ' $1
+	echo 'Second input: ' $2
+	echo 'Third input: ' $3
+	echo 'Fourth input: ' $4
+    echo 'Process id: ' pidof $0
+    echo 'Total number of input parameters: ' $#
+else
+    echo 'Usage: ./scriptname.sh first second third fourth'
+fi
+
